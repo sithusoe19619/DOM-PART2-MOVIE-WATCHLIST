be0b56364d7b7c066060acc43a558679732d22e5
Sithu Soe <sithusoe19619@gmail.com>
2026-06-11 13:33:55 -0400

    phase 1: select DOM elements

---
+++ movie-watchlist/script.js
@@ -0,0 +1,26 @@
+const appTitle = document.getElementById("app-title")
+const movieCount = document.getElementById("movie-count")
+const movieForm = document.getElementById("movie-form")
+const titleInput = document.getElementById("title-input")
+const genreInput = document.getElementById("genre-input")
+const movieList = document.getElementById("movie-list")
+const clearWatchedBtn = document.getElementById("clear-watched-btn")
+
+const filterBtns = document.querySelectorAll("filter-btn")
+// select #movie-form        → store in movieForm
+// select #title-input       → store in titleInput
+// select #genre-input       → store in genreInput
+// select #movie-list        → store in movieList
+// select #clear-watched-btn → store in clearWatchedBtn
+
+// select ALL elements with class "filter-btn" using querySelectorAll
+// store them in filterBtns — you'll loop over them in Phase 6
+
+console.log(appTitle)
+console.log(movieCount)
+console.log(movieForm)
+console.log(titleInput)
+console.log(genreInput)
+console.log(movieList)
+console.log(clearWatchedBtn)
+console.log(filterBtns)
